2baaf13a8fa23e5694ffa8b49c0a61aeeec062cb
Joe Lizardo Diaz Echevarria <joediaz88@hotmail.com>
2025-10-28 11:31:00 +0000

    installed react vite, imported fonts, removed default react-vite  elements

---
+++ martina_papier_psicologa/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Martina Papier </h1>
+      <h3>Psicóloga</h3>
+      <p>Especializada en <b>TCA</b> </p>
+    </>
+  )
+}
+
+export default App

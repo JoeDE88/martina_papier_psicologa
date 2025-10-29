2ddc54a190495d3376f4adfd65a6dde407cf9a4c
JoEDE88 <joediaz88@hotmail.com>
2025-10-29 15:27:32 +0100

    added bootstrap, created navbar, modified navbar styles

--- martina_papier_psicologa/src/components/NavBar.jsx
+++ martina_papier_psicologa/src/components/NavBar.jsx
@@ -1,20 +1,41 @@
-export default function NavBar(){
-    return (
-        <>
-        <nav className="navbar">
-            <div className="navbar-left">
-                <a href="/" className="logo">Logo MPP</a>
-            </div>
-            <div className="navbar-center">
-                <ul className="navbar-links">
-                    <li><a href="">Inicio</a></li>
-                    <li><a href="">Acerca de mí</a></li>
-                    <li><a href="">Mis servicios</a></li>
-                    <li><a href="">Contacto</a></li>
-                </ul>
-            </div>
-            <div className="navbar-right">Hamburger</div>
-        </nav>
-        </>
-    )
+export default function NavBar() {
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg">
+          <a class="navbar-brand logo" href="#">
+            MPP
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i className="bi bi-list"></i>
+          </button>
+          <div class="collapse navbar-collapse justify-content-between" id="navbarTogglerDemo02">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Acerca de mí
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Mis servicios
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Contacto
+                </a>
+              </li>
+            </ul>
+          </div>
+      </nav>
+    </>
+  );
 }

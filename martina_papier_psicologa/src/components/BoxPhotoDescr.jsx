fb2335d8fc18cc4b06334d9afb59977d8bac85e0
JoeDE88 <joediaz88@hotmail.com>
2026-04-12 13:13:30 +0200

    converted PhotoAndDescr comp in box, created ProPic comp

--- martina_papier_psicologa/src/components/BoxPhotoDescr.jsx
+++ martina_papier_psicologa/src/components/BoxPhotoDescr.jsx
@@ -1,21 +1,21 @@
-import Martu_pic from '../assets/Martu_pic.jpg'
+import ProPic from './ProPic';
 
-export default function PhotoAndDescription() {
+export default function BoxPhotoDescr() {
   return (
     <div className="container">
       <div className="row mt-5 mb-5 d-flex justify-content-center">
         <div className="col-12 col-lg-5 mb-lg-0 foto-card">
-          <img
-            src={Martu_pic}
-            alt="Martina Papier pic"
-            className="img profile-img"
-          />
+          <ProPic />
         </div>
         <div className="col-12 col-lg-5 text-container">
           <h1>Martina Papier</h1>
-          <h3>
+          <h2>
             Psicóloga especializada en{" "}
-            <b>Trastornos de la Conducta Alimentaria</b>
+          </h2>
+          <h3>
+            <b>
+              Trastornos de la Conducta Alimentaria
+            </b>
           </h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam
---
+++ martina_papier_psicologa/src/components/ProPic.jsx
@@ -0,0 +1,11 @@
+import Martu_pic from '../assets/Martu_pic.jpg'
+
+export default function ProPic() {
+    return (
+        <img
+            src={Martu_pic}
+            alt="Martina Papier pic"
+            className="img profile-img"
+        />
+    )
+}

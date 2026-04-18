19f4f89a044cbb87138d54bd0ea75fcb4b74caef
JoeDE88 <joediaz88@hotmail.com>
2026-04-19 00:37:50 +0200

    added constant dir and strings.js file

---
+++ martina_papier_psicologa/src/constants/strings.js
@@ -0,0 +1,14 @@
+export const MARTINA = {
+  name: "Martina Papier",
+  profession: "Psicóloga especializada",
+  qualification: "Trastornos de la Conducta Alimentaria",
+  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam
+            lacus, lacinia eget sem vel, condimentum placerat ipsum. Fusce quis
+            pretium mi, at sagittis eros. Mauris auctor tellus turpis, non
+            blandit elit efficitur non. Phasellus id elit lectus. Pellentesque
+            ut urna purus. Suspendisse aliquam est nec velit congue hendrerit.
+            Proin vehicula semper nunc sed bibendum. Donec laoreet rutrum
+            libero. Proin egestas vehicula semper. Fusce quis pretium mi, at
+            sagittis eros. Mauris auctor tellus turpis, non blandit elit
+            efficitur non.`,
+};
